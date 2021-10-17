951a63c61dd534cdefd87d4d3c3a8a7560ca143f
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2021-10-17 18:09:24 -0300

    feat: add styled

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+export default {
+  colors: {
+    shape: '#FFFFFF',
+    primary: {
+      50: '#FAF9FF',
+      100: '#50427B',
+      200: '#3A2A6A',
+    },
+    secondary: {
+      300: '#3DC1CD',
+    },
+    text: {
+      primary: '#2E2553',
+      secondary: '#514F5A'
+    }
+  },
+  shadows: {
+    android: {
+      'primary-inverse': 10,
+    },
+    ios: {
+      'primary-inverse': '0px -4px 10px -2px rgba(255, 255, 255, 0.25)',
+    }
+  },
+  fonts: {
+    regular: 'Roboto_400',
+    medium: 'Roboto_500',
+    bold: 'Roboto_700',
+  } 
+}
